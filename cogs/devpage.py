f46696aceae8b0b3e1017dd39cde80301ad9d996
Aliz <104936546+TSCOfficial@users.noreply.github.com>
2023-09-02 15:31:01 +0200

    Update 23.0902.15

--- cogs/devpage.py
+++ cogs/devpage.py
@@ -1,11 +1,16 @@
 import discord
 from discord.ext import commands
+from discord.app_commands import AppCommandError, CommandNotFound, MissingPermissions, Choice
+from discord import app_commands
 
 class devpage(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
 
-
+    @app_commands.command(name="msg", description="Sende eine Nachricht")
+    async def msg(self, interaction:discord.Interaction, nachricht:str):
+        await interaction.response.send_message(content=nachricht)
+        return
 
 async def setup(bot):
     await bot.add_cog(devpage(bot))
--- cogs/settings/autothread.py
+++ cogs/settings/autothread.py
@@ -1,3 +1,4 @@
+from email import message
 import discord
 from discord.ext import commands
 from discord.utils import get
@@ -8,13 +9,27 @@ import mysql.connector
 import config as config
 from config import conf
 
-async def get_autothread_overview(interaction: discord.Interaction):
+async def get_autothread_overview(interaction: discord.Interaction, channel:int=None):
     embed = await conf().EMBED(
         interaction=interaction,
         title="Auto Thread",
         description="Bearbeite die Auto Thread EInstellungen des Servers.",
     )
     embed.add_field(name="Auto Thread", value="Thread Einstellungen ändern.\ncmd", inline=False)
+
+    connection = await conf().DB_con()
+    cursor = connection.cursor()
+    if channel is not None:
+        cursor.execute(f"SELECT guild, thread_channel, thread_name, thread_message, thread_minchars, thread_cooldown, thread_exceptbot FROM thread WHERE thread_channel = {channel}")
+    else:
+        cursor.execute(f"SELECT guild, thread_channel, thread_name, thread_message, thread_minchars, thread_cooldown, thread_exceptbot FROM thread WHERE guild = {interaction.guild.id}")
+    result = cursor.fetchall()# -> [(guild, channel, ..)] 
+    if result != list():
+        for thread in result:
+            print(thread)
+            embed.add_field(name="Thread setting", value=thread, inline=False)
+
+    
     return embed
 
 class autothread(commands.GroupCog, name="thread"):
@@ -36,26 +51,34 @@ class autothread(commands.GroupCog, name="thread"):
             description="Bearbeite die Auto Thread Einstellungen",
             )
     @app_commands.describe(
-        name="Thread Name eingeben (Siehe Thread Übersicht für variabeln)",
+        channel="Kanal eingeben inwelchem Auto Threading aktiviert sein soll*",
+        name="Thread Name eingeben (Siehe Thread Übersicht für variabeln)*",
         nachricht="Erste Thread Nachricht eingeben",
         charachter="Mindest Charachter Limit für Threadestellung eingeben",
         cooldown="Schreibcooldown in Sekunden eingeben",
-        channel="Kanal eingeben inwelchem Auto Threading aktiviert sein soll.",
+        bots="Erlaube das Threading für Bot Nachrichten"
         )
+    @app_commands.choices(
+        bots=[
+            Choice(name="Ja, Bots einbinden", value=1),
+            Choice(name="Nein, Bots ausschliessen", value=0)
+        ]
+    )
     async def thread_autothread(self, interaction: discord.Interaction,
-                              name:str=None,
-                              nachricht:str=None,
-                              charachter:int=None,
-                              cooldown:int=None,
-                              channel:discord.TextChannel=None):
+                                channel:discord.TextChannel=None,
+                                name:str=None,
+                                nachricht:str=None,
+                                charachter:int=None,
+                                cooldown:int=None,
+                                bots:int=0
+                                ):
         from cogs.settings.settingpage import goto_settinghome
         embed = await get_autothread_overview(interaction=interaction)
         connection = await conf().DB_con()
         cursor = connection.cursor()
         cursor.execute(f"SELECT * FROM thread WHERE guild = {interaction.guild.id}")
-        result = cursor.fetchall()
-        print(result)
-        print(type(result))        
+        result = cursor.fetchall()# -> [(guild, channel)]
+        print(result)      
         if name is None and nachricht is None and charachter is None and cooldown is None and channel is None:
             await interaction.response.send_message(embed=embed, view=goto_settinghome())
             return
@@ -67,17 +90,34 @@ class autothread(commands.GroupCog, name="thread"):
                 view=goto_settinghome())
             return
         
-        elif result == list():
-            sql = "INSERT INTO thread (guild, thread_channel, thread_name, thread_minchars, thread_cooldown) VALUES (%s, %s, %s, %s, %s)"
-            val = (interaction.guild.id, channel.id, name, charachter, cooldown)
-            cursor.execute(sql, val)
-            connection.commit()
-            await interaction.response.send_message(content="inserted")
-            return
-        
+    
         else:
-            print("Error thread_autothread")
-            return
+            if len(result) >= config.LIMIT_AUTOTHREAD:
+                await interaction.response.send_message(
+                content=f"Das Autothread Limit ({config.LIMIT_AUTOTHREAD}) wurde erreicht!", 
+                embed=embed,
+                view=goto_settinghome())
+                return
+            cursor.execute(f"SELECT guild, thread_channel FROM thread WHERE guild = {interaction.guild.id} AND thread_channel = {channel.id}")
+            result = cursor.fetchall()# -> [(guild, channel)] 
+            print(result)  
+            if result == list():
+                sql = "INSERT INTO thread (guild, thread_channel, thread_name, thread_message, thread_minchars, thread_cooldown, thread_exceptbot) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                val = (interaction.guild.id, channel.id, name, nachricht, charachter, cooldown, bots)
+                cursor.execute(sql, val)
+                connection.commit()
+                await interaction.response.send_message(content="inserted")
+                return
+            
+            elif interaction.guild.id == result[0][0] and channel.id == result[0][1]:
+                charachter = "NULL" if charachter is None else charachter
+                cooldown = "NULL" if cooldown is None else cooldown
+                cursor.execute(f"UPDATE thread SET `thread_name`='{name}', `thread_message`='{nachricht}', `thread_minchars`={charachter}, `thread_cooldown`={cooldown}, `thread_exceptbot`={bots} WHERE `guild`={interaction.guild.id} AND `thread_channel`={channel.id}")
+                connection.commit()
+                await interaction.response.send_message(content="updated")
+            else:
+                print("Error thread_autothread")
+                return
     
     @app_commands.command(
             name="autorename",
@@ -112,23 +152,28 @@ class autothread_threadrename(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        print(message)
+        print(message.content)
         connection = await conf().DB_con()
         cursor = connection.cursor()
-        cursor.execute(f"SELECT guild, thread_name, thread_message, thread_minchars, thread_cooldown, thread_channel FROM thread")
-        result = list(cursor.fetchone())
+        cursor.execute(f"SELECT guild, thread_name, thread_message, thread_minchars, thread_cooldown, thread_channel, thread_exceptbot FROM thread")
+        result = list(cursor.fetchall())
+        i = 0
+        for thread in result:
+            name_module = result[i][1]
+            firstmsg_module = result[i][2]
+            char_min_limit = int(result[i][3]) if result[i][3] else 0
+            slowmode_module = int(result[i][4]) if result[i][4] else 0
+            channel = result[i][5]
+            bots = result[i][6] #1 = True, 0 = False
 
-        name_module = result[1]
-        firstmsg_module = result[2]
-        char_min_limit = int(result[3]) if result[3] else None
-        slowmode_module = int(result[4]) if result[4] else None
-        channel = result[5]
-
-        if message.channel.id == channel and len(message.content) > char_min_limit:      
-            thread = await message.create_thread(name=name_module, reason="AutoThread", slowmode_delay=slowmode_module)
-            await thread.send(firstmsg_module)
-            return
-    
+            if message.channel.id == channel and len(message.content) > char_min_limit:
+                if not message.author.bot or (message.author.bot and bots == 1): #user is not bot, or user is bot and bot allowed
+                    thread = await message.create_thread(name=name_module, reason="AutoThread", slowmode_delay=slowmode_module)
+                    if firstmsg_module is not None:
+                        await thread.send(firstmsg_module)
+            i += 1
+            
+        
 async def setup(client):
     await client.add_cog(autothread(client))
     await client.add_cog(autothread_threadrename(client))
